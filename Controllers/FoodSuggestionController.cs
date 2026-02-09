19d32ba829afebac6fe0a0b5c101f63459fdc3d0
jamel <jamel@JAMELS_LAPTOP>
2026-02-08 19:50:32 -0800

    week 5

---
+++ Controllers/FoodSuggestionController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FoodTruck.Models;
+
+
+namespace FoodTruck.Controllers
+{
+    public class FoodSuggestionController : Controller
+    {
+       
+        [HttpPost]
+        public IActionResult Create(FoodSuggestionModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            // Success logic will be expanded later in the course (CRUD + database persistence).
+            // For now, we simply redirect to a success page (PRG).
+            return RedirectToAction("ThankYou");
+        }
+
+        // GET: /Suggestions/ThankYou
+        public IActionResult ThankYou()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/FoodSuggestionModel.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace FoodTruck.Models
+{
+    public class FoodSuggestionModel
+    {
+        public class foodSuggestionModel
+        {
+            [Required]
+            [StringLength(80)]
+            public string Id { get; set; }
+
+            [Required]
+            [StringLength(60)]
+            public string Town { get; set; }
+
+            [Range(1, 10)]
+            public int Street { get; set; }
+
+            [StringLength(300)]
+            public string EmeilAddress { get; set; }
+        }
+    }
+}
